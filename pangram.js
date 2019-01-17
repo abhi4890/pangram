babf949c2ea40175a9426c48e8cd2620fa29116c
Abhishek Katiyar <abhishekkatiyar049@gmail.com>
2019-01-17 10:15:19 +0530

    changes for pangram check

--- pangram.js
+++ pangram.js
@@ -1,3 +1,6 @@
-export const isPangram = () => {
-  return false;
+export const isPangram = (sentence) => {
+  let string;
+  sentence = sentence.toLowerCase();
+  string = 'abcdefghijklmnopqrstuvwxyz';
+  return string.split('').every(letter => sentence.includes(letter));
 };
